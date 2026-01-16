79d76752b98110f25addcc09b4b447983ad6379b
Aditya Anand <adityaanand1365@gmail.com>
2026-01-16 23:13:51 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Dashboad from "./Dashboad";
+import TopBar from "./TopBar";
+
+const Home = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboad />
+    </>
+  );
+};
+
+export default Home;
